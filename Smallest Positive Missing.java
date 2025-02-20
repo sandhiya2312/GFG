836fd598baff3bf8389f94b06468af99254f4bbc
sandhiya2312 <165649463+sandhiya2312@users.noreply.github.com>
2025-02-20 21:58:34 +0530

    Smallest Positive Missing.java

---
+++ Smallest Positive Missing.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0 && arr[i] <= n)
+                vis[arr[i] - 1] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i - 1]) {
+                return i;
+            }
+        }
+
+       
+        return n + 1;
+    }
+}
